be1892b76f2661bdcdb233ff10f2c5c7072e161f
eldarhakimov <el.hackimov@gmail.com>
2024-09-21 17:14:09 +0300

    Create new module test for add ingredient in class Burger

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -14,5 +14,23 @@ class TestBurger:
         burger.set_buns(mock_bun)
         assert burger.bun == mock_bun
 
+    @pytest.mark.parametrize('expected_result', ['Мясо бессмертных моллюсков Protostomia',
+                                                 'Говяжий метеорит (отбивная)',
+                                                 'Биокотлета из марсианской Магнолии',
+                                                 'Филе Люминесцентного тетраодонтимформа',
+                                                 'Хрустящие минеральные кольца',
+                                                 'Плоды Фалленианского дерева',
+                                                 'Кристаллы марсианских альфа-сахаридов',
+                                                 'Мини-салат Экзо-Плантаго',
+                                                 'Сыр с астероидной плесенью'])
+    def test_add_ingredient(self, expected_result):
+        mock_ingredient = Mock()
+        mock_ingredient.ingredient = expected_result
+        burger = Burger()
+        burger.add_ingredient(mock_ingredient)
+        ingredient_values = []
+        for ing in burger.ingredients:
+            ingredient_values.append(ing.ingredient)
+        assert expected_result in ingredient_values
 
 
