f4f43feaf5bc54c487c11dfe08c2e27f7fbc7fac
eldarhakimov <el.hackimov@gmail.com>
2024-10-05 16:10:19 +0300

    Create new test modules for test name ingredient

---
+++ tests/test_ingredient.py
@@ -0,0 +1,14 @@
+import pytest
+from praktikum.ingredient import Ingredient
+from praktikum.ingredient_types import *
+
+
+class TestIngredient:
+
+    @pytest.mark.parametrize('type_ingredient,name_ingredient,price', [[INGREDIENT_TYPE_SAUCE, 'Соус Spicy-X', 90],
+                                                          [INGREDIENT_TYPE_FILLING, 'Плоды Фалленианского дерева', 874],
+                                                 [INGREDIENT_TYPE_SAUCE, 'Соус с шипами Антарианского плоскоходца', 88],
+                                                        [INGREDIENT_TYPE_FILLING, 'Говяжий метеорит (отбивная)', 3000]])
+    def test_get_exact_name(self, type_ingredient, name_ingredient, price):
+        ingredient = Ingredient(type_ingredient, name_ingredient, price)
+        assert ingredient.get_name() == name_ingredient
